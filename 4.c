680c932e85787fd0b4c49ea57ec9469fb3c86c52
natindo <lyskov256@gmail.com>
2021-09-23 15:01:37 +0300

    with problems

--- 4.c
+++ 4.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 #include <math.h>
+#include <ctype.h>
 
 int main() {
 
     char c;
     char a[64] = {};
-    int i = 0;
-
-    while ((c = getchar()) != '\n' && i<=64)
+    int i = 0, j = 0;
+    //Reading the data
+    while ((c = getchar()) != '\n') {
         a[i++] = c;
+        if (i > 64) { //Checking the correctness of the string length
+            printf("Введены слышком большие значения\n");
+            return 1;
+        }
+    }
+
+    while (j != i) {
+        if (isdigit(a[j]) == 0 || a[j] != '|') {
+            printf("%c\n", a[j]);
+            printf("Не вводите буквы\n");
+            return 3;
+        }
+        j++;
+    } 
 
     int counter = 0;
 
@@ -16,12 +31,17 @@ int main() {
         counter++;
     }
 
-    double res, res1 = 0;
+    if(counter = i) {
+        printf ("Не была введена вертикальная черта\n");
+        return 2;
+    }
+
+    double res = 0, res1 = 0;
     i = counter - 1;
     int g = 0;
 
     while (i >= 0) {
-        res1 = pow(10, g) * (a[i] - '0');
+        res1 = res1 + (pow(10, g) * (a[i] - '0'));
         i--;
         g++;
     }
@@ -31,14 +51,12 @@ int main() {
     double basis = 0.1;
 
     while (a[counter] != '\0') {
-        res = res1 + pow(basis, g) * (a[counter] - '0');
-        //printf ("%lf\n", pow((0,1), (g)));
-        //printf ("%d\n", counter);
+        res = res1 + (pow(basis, g) * (a[counter] - '0'));
         counter++;
         g++;
     }
 
-    printf("%g", res);
+    printf("%g\n", res);
 
     return 0;
 }
